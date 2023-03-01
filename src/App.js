0efdcc7804b01863efd3109b31bdc00ee9dcbdc2
sudais <sudais.shafiq22@gmail.com>
2023-03-01 17:29:09 +0500

    adding items to the list

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({show: false, msg:'', type:''});
+  const handleSubmit = e=>{
+        e.preventDefault();
+        if(!name){
+          // display alert
+          showAlert(true, 'danger', 'Please enter value');
+        }else if (name && isEditing){
+          setAlert({show: true, msg:'You are editing the item', type: 'success' })
+        }else {
+          showAlert(true, 'success', 'item added to the list');
+          const newItem = {id: new Date().getTime().toString(), title: name};
+          setList([...list, newItem]);
+          setName('');
+        }
+  }
+
+  const showAlert = (show = false, type= '', msg='')=>{
+    setAlert({show, type, msg})
+  }
+
+  const clearList=()=>{
+    showAlert(true, 'danger', 'empty list');
+    setList([]);
+  }
+
+  return (
+  <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      {alert.show && <Alert {...alert} removeAlert={showAlert}/>}
+      <h3>grocery bud</h3>
+      <div className="form-control">
+        <input type="text" className="grocery" placeholder='e.g. eggs' value={name} onChange= {e=>setName(e.target.value)} />
+        <button type="submit" className='submit-btn'>{isEditing? 'edit' : 'submit'}</button>
+      </div>
+    </form>
+    {list.length> 0 && (<div className="grocery-container">
+      <List items={list}/>
+      <button className="clear-btn" onClick={clearList}>clear items</button>
+    </div>)}
+  </section>
+)}
+
+export default App
